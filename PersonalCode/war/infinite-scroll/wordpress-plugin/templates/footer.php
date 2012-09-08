ebb455cf922b90e6fa690ae7e4ff5fa9a58758b6
angge3 <angge3@gmail.com>
2012-09-08 22:03:55 +0800

    handle delete files

--- PersonalCode/war/infinite-scroll/wordpress-plugin/templates/footer.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Template to include on wp_footer front-end; passes plugin options to javascript function
- * @package Infinite_Scroll
- */
-?>
-<script>jQuery( infinite_scroll.contentSelector ).infinitescroll( infinite_scroll );</script>
